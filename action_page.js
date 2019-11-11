83030e8d6fe1e028ccad5eea59769dcc5cb0f3b8
Postbox2 <53163105+host12prog@users.noreply.github.com>
2019-11-11 17:56:28 +0700

    Create action_page.js

---
+++ action_page.js
@@ -0,0 +1,18 @@
+var x = document.getElementById("");
+var y = document.getElementById("");
+
+function makepass(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+x.onclick = function() {
+    var s = document.createElement("strong");
+    s.innerText = "Your generated password is: " + makepass(y.value);
+    document.body.appendChild(s);
+}
